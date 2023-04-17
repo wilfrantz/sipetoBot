027b13980115b6c1533cbc5d8b62f3dd1444a7ea
wilfrantz <wilfrantz@dede.dev>
2023-04-17 20:02:34 +0000

    Sets twitter structure, compiles and runs.

---
+++ src/include/td_header.h
@@ -0,0 +1,9 @@
+#ifndef TD_HEADER_H
+#define TD_HEADER_H
+
+#include <td/telegram/td_api.h>
+#include <td/telegram/td_log.h>
+#include <td/telegram/Client.h>
+#include <td/telegram/td_json_client.h>
+
+#endif // !TD_HEADER_H
